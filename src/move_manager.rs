8b0c9e61de66aafc920ae72bde66480e9bb3db83
dario <dario.fila@gmail.com>
2023-11-17 13:51:36 +0100

    chore: upload pgn file

--- src/move_manager.rs
+++ src/move_manager.rs
@@ -1,5 +1,26 @@
+use crate::r#move::Move;
 
+pub struct MoveManager{
 
-struct MoveManager{
+}
+
+impl MoveManager{
+
+    pub fn new() -> MoveManager{
+        MoveManager{}
+    }
+
+    pub fn get_move(&self, move_str: &str) -> Move{
+        let mut move_str = move_str.to_string();
+        move_str.make_ascii_lowercase();
+        match move_str.as_str(){
+            "rock" => Move::Rock,
+            "paper" => Move::Paper,
+            "scissors" => Move::Scissors,
+            "lizard" => Move::Lizard,
+            "spock" => Move::Spock,
+            _ => Move::Invalid
+        }
+    }
 
 }
