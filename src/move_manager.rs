36a3492ca0ce0fa7da14ecabaa0183eec4852b0d
MattiaSC01 <msca@bendingspoons.com>
2023-11-18 11:59:23 +0100

    feat: move order

--- src/move_manager.rs
+++ src/move_manager.rs
@@ -14,34 +14,47 @@ fn move_score(m: &Move) -> i32 {
 }
 
 pub struct MoveManager {
-    quite_moves: Vec<Move>,
+    quiet_moves: Vec<Move>,
     capture_moves: Vec<Move>,
+    priority_moves: Vec<Move>,
 }
 
 impl MoveManager {
     pub fn new() -> MoveManager {
-        MoveManager { quite_moves: vec![], capture_moves: vec![] }
+        MoveManager { quiet_moves: vec![], capture_moves: vec![], priority_moves: vec![] }
     }
 
     pub fn add_move(&mut self, m: Move) {
         if m.piece_captured != PieceType::Null || m.is_en_passant {
             self.capture_moves.push(m);
         } else {
-            self.quite_moves.push(m);
+            self.quiet_moves.push(m);
         }
     }
 
+    // must be called in reversed order of priority
+    pub fn add_priority_move(&mut self, m: Move) {
+        let l = self.len();
+        self.quiet_moves.retain(|x| *x != m);
+        self.capture_moves.retain(|x| *x != m);
+
+        if self.len() != l {
+            self.priority_moves.push(m);            
+        }
+
+    }   
+
     pub fn sort(&mut self) {
-        self.quite_moves.sort_by_key(|a| -move_score(a) );
+        self.quiet_moves.sort_by_key(|a| -move_score(a) );
         self.capture_moves.sort_by_key(|a| -move_score(a) );
     }
 
     pub fn iter(&self) -> impl Iterator<Item=&Move> {
-        return self.capture_moves.iter().chain(self.quite_moves.iter());
+        return self.priority_moves.iter().chain(self.capture_moves.iter().chain(self.quiet_moves.iter()));
     }
 
     pub fn len(&self) -> usize {
-        return self.quite_moves.len() + self.capture_moves.len();
+        return self.quiet_moves.len() + self.capture_moves.len() + self.priority_moves.len();
     }
 }
 
